e3d98bfc469b7cc8dd212286c0f30a8f67864d0d
최사라 <64395351+withHappy@users.noreply.github.com>
2020-07-10 07:50:36 +0900

    Revert "Feat post"

--- happygram/core/urls.py
+++ happygram/core/urls.py
@@ -1,12 +1,9 @@
 from rest_framework.routers import SimpleRouter
-from posts.views import PostViewSet
+
 from profiles.views import ProfileViewSet
 from users.views import UserViewSet
 
 router = SimpleRouter(trailing_slash=False)
-
 router.register(r'users', UserViewSet)
 router.register(r'profiles', ProfileViewSet)
-router.register(r'posts', PostViewSet)
-
 urlpatterns = router.urls
