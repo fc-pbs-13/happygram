7560ca3617fe9f9747c3c16126c60b7a06667608
withHappy <hap2.dev@gmail.com>
2020-07-15 11:17:11 +0900

    feat : add relations testcase

--- happygram/relations/serializers.py
+++ happygram/relations/serializers.py
@@ -1,6 +1,14 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
 
 from relations.models import Relation
+
+
+class CustomUniqueTogetherValidator(UniqueTogetherValidator):
+    def enforce_required_fields(self, attrs, serializer):
+        attrs['from_user'] = serializer.context['request'].user.id
+        attrs['to_user'] = serializer.context['request'].data['to_user']
+        super().enforce_required_fields(attrs, serializer)
 
 
 class RelationSerializer(serializers.ModelSerializer):
@@ -8,3 +16,9 @@ class RelationSerializer(serializers.ModelSerializer):
         model = Relation
         fields = ('id', 'to_user', 'related_type', 'from_user')
         read_only_fields = ('from_user',)
+        validators = [
+            CustomUniqueTogetherValidator(
+                queryset=Relation.objects.all(),
+                fields=('from_user', 'to_user')
+            )
+        ]
--- happygram/relations/tests.py
+++ happygram/relations/tests.py
@@ -3,6 +3,8 @@ from munch import Munch
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+from relations.models import Relation
+
 
 class UserRelationTestCase(APITestCase):
     def setUp(self) -> None:
@@ -12,14 +14,46 @@ class UserRelationTestCase(APITestCase):
             'related_type': 'f',
         }
 
-    def test_user_follow(self):
+    def test_relations_create(self):
         self.client.force_authenticate(user=self.users[0])
+
         response = self.client.post('/api/relations', data=self.data)
-        print(response.data)
+
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         follow_response = Munch(response.data)
         self.assertEqual(follow_response.to_user, self.data['to_user'])
         self.assertEqual(follow_response.related_type, self.data['related_type'])
         self.assertEqual(follow_response.from_user, self.users[0].id)
+
+    def test_relations_destroy(self):
+        relation = Relation.objects.create(from_user=self.users[0], to_user=self.users[1], related_type='f')
+
+        response = self.client.delete(f'/api/relations/{relation.id}')
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        self.assertFalse(Relation.objects.filter(pk=relation.id).exists())
+
+    def test_relations_update(self):
+        relation = Relation.objects.create(from_user=self.users[0], to_user=self.users[1], related_type='f')
+
+        update_data = {
+            'related_type': 'b'
+        }
+        response = self.client.patch(f'/api/relations/{relation.id}', data=update_data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        follow_response = Munch(response.data)
+        self.assertEqual(follow_response.related_type, update_data['related_type'])
+
+    def test_relations_duplicate(self):
+        Relation.objects.create(from_user=self.users[0], to_user=self.users[1], related_type='b')
+
+        self.client.force_authenticate(user=self.users[0])
+
+        response = self.client.post(f'/api/relations', data=self.data)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST, response.data)
 
