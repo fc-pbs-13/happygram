b02a09252f7282fca53057232495f903dbb108c5
withHappy <hap2.dev@gmail.com>
2020-07-24 15:26:25 +0900

    feat : add testcase

--- happygram/posts/admin.py
+++ happygram/posts/admin.py
@@ -1,4 +1,8 @@
+import csv
+
 from django.contrib import admin
+from django.http import HttpResponse
+from django.utils.safestring import mark_safe
 from mptt.admin import MPTTModelAdmin
 from taggit.models import Tag
 
@@ -31,10 +35,37 @@ class LikeAdmin(admin.ModelAdmin):
     list_display = ['id', 'post_id', 'user_id']
     search_fields = ['post__id', 'user__email']
 
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
+
 
 @admin.register(Photo)
-class PhotoAdmin(admin.ModelAdmin):
+class PhotoAdmin(admin.ModelAdmin, ExportCsvMixin):
     list_display = ['id', 'image', 'post_id']
+    readonly_fields = ['headshot_image']
+
+    def headshot_image(self, obj):
+        return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
+            url=obj.image.url,
+            width=obj.image.width,
+            height=obj.image.height,
+        )
+        )
 
 
 class MyModelAdmin(admin.ModelAdmin):
