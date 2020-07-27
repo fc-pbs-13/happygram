01866a338b9102efbf6d8ae4bc3eca895e50bf18
withHappy <hap2.dev@gmail.com>
2020-07-27 16:14:39 +0900

    update : session_authentication

--- happygram/happygram/settings/base.py
+++ happygram/happygram/settings/base.py
@@ -141,7 +141,9 @@ MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '../../images')
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.TokenAuthentication'],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication'],
     'DEFAULT_PAGINATION_CLASS': 'core.pagination.Pagination',
     'PAGE_SIZE': 5,
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated'],
